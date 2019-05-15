00af7ee30da53d78a0a2eda87d2d5281d34b0611
Petar.Ilijasic <Petar.Ilijasic@avl.com>
2019-05-15 16:14:19 +0200

    Added measurement recorder

--- VS2019_version/AVLCarMeasurementDemo/AVLCarMeasurementDemo/Controllers/MeasurementsController.cs
+++ VS2019_version/AVLCarMeasurementDemo/AVLCarMeasurementDemo/Controllers/MeasurementsController.cs
@@ -11,41 +11,46 @@ namespace AVLCarMeasurementDemo.Controllers
 {
   [EnableCors("AllowOrigin")]
   public class MeasurementsController : ODataController
+  {
+    private CarMeasurementContext _db;
+
+
+    public MeasurementsController(CarMeasurementContext context)
     {
-        private CarMeasurementContext _db;
-
-        public MeasurementsController(CarMeasurementContext context)
+      _db = context;
+      if (context.Measurements.Count() == 0)
+      {
+        foreach (var b in DataSource.GetMeasurements())
         {
-            _db = context;
-            if (context.Measurements.Count() == 0)
-            {
-                foreach (var b in DataSource.GetMeasurements())
-                {
-                    context.Measurements.Add(b);
-                }
-                context.SaveChanges();
-            }
+          context.Measurements.Add(b);
         }
-        
-        [EnableQuery]
-        public IActionResult Get()
-        {
-            return Ok(_db.Measurements);
-        }
-
-        [EnableQuery]
-        public IActionResult Get(int key)
-        {
-            return Ok(_db.Measurements.FirstOrDefault(c => c.ID == key));
-        }
-
-        [EnableQuery]
-        public IActionResult Post([FromBody]Measurement measurement)
-        {
-            _db.Measurements.Add(measurement);
-            _db.SaveChanges();
-            return Created(measurement);
-        }
+        context.SaveChanges();
+      }
     }
+
+
+    [EnableQuery]
+    public IActionResult Get()
+    {
+      DataGenerator generator = new DataGenerator(_db);
+      generator.RecordNewData();    
+      
+      return Ok(_db.Measurements);
+    }
+
+    [EnableQuery]
+    public IActionResult Get(int key)
+    {
+      return Ok(_db.Measurements.FirstOrDefault(c => c.ID == key));
+    }
+
+    [EnableQuery]
+    public IActionResult Post([FromBody]Measurement measurement)
+    {
+      _db.Measurements.Add(measurement);
+      _db.SaveChanges();
+      return Created(measurement);
+    }
+  }
 }
 
---
+++ VS2019_version/AVLCarMeasurementDemo/AVLCarMeasurementDemo/DataGenerator.cs
@@ -0,0 +1,97 @@
+﻿using AVLCarMeasurementDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AVLCarMeasurementDemo
+{
+  public class DataGenerator
+  {
+
+    CarMeasurementContext _db;
+
+    public DataGenerator(CarMeasurementContext _db)
+    {
+      this._db = _db;
+    }
+
+    public void RecordNewData()
+    {
+      int measurementId = _db.Measurements.Last<Measurement>().ID + 1;
+      double time = _db.Measurements.Last<Measurement>().Time + 0.5;
+      Random r = new Random();
+      double range = 500;
+
+      _db.Measurements.Add(new Measurement
+      {
+        ID = measurementId,
+        UUT = "BMW M2",
+        CT = "CO2 Emission",
+        Time = time,
+        Value = r.NextDouble() * range
+      });
+
+      ++measurementId;
+      time += 0.5;
+
+      _db.Measurements.Add(new Measurement
+      {
+        ID = measurementId,
+        UUT = "BMW M2",
+        CT = "CO2 Emission",
+        Time = time,
+        Value = r.NextDouble() * range
+      });
+
+      ++measurementId;
+      time += 0.5;
+
+      _db.Measurements.Add(new Measurement
+      {
+        ID = measurementId,
+        UUT = "BMW M2",
+        CT = "CO2 Emission",
+        Time = time,
+        Value = r.NextDouble() * range
+      });
+
+      ++measurementId;
+      time += 0.5;
+
+      _db.Measurements.Add(new Measurement
+      {
+        ID = measurementId,
+        UUT = "Audi Q8",
+        CT = "CO2 Emission",
+        Time = time,
+        Value = r.NextDouble() * range
+      });
+
+      ++measurementId;
+      time += 0.5;
+
+      _db.Measurements.Add(new Measurement
+      {
+        ID = measurementId,
+        UUT = "Audi Q8",
+        CT = "CO2 Emission",
+        Time = time,
+        Value = r.NextDouble() * range
+      });
+
+      ++measurementId;
+      time += 0.5;
+
+      _db.Measurements.Add(new Measurement
+      {
+        ID = measurementId + 1,
+        UUT = "Audi Q8",
+        CT = "CO2 Emission",
+        Time = time,
+        Value = r.NextDouble() * range
+      });
+      _db.SaveChanges();
+    }
+  }
+}
